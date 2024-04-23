b0db7f594dac8524ea51f91b05c78a9d7afe1e9d
sebbex1337 <sefort@hotmail.dk>
2024-04-23 10:27:50 +0200

    init

---
+++ src/test/java/minkavlerne/cafekeabackend/CafeKeaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package minkavlerne.cafekeabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeKeaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
