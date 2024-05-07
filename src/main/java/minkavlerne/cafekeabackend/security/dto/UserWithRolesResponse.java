ab6942746eb579c645b34be041e7475fe782db8d
sebbex1337 <sefort@hotmail.dk>
2024-05-07 10:06:01 +0200

    switched to using CoffeeDto

--- src/main/java/minkavlerne/cafekeabackend/security/dto/UserWithRolesResponse.java
+++ src/main/java/minkavlerne/cafekeabackend/security/dto/UserWithRolesResponse.java
@@ -3,6 +3,7 @@ package minkavlerne.cafekeabackend.security.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import minkavlerne.cafekeabackend.cafe.dto.CoffeeDto;
 import minkavlerne.cafekeabackend.cafe.dto.TicketDto;
 import minkavlerne.cafekeabackend.cafe.entity.CustomerTicket;
 import minkavlerne.cafekeabackend.cafe.entity.Ticket;
@@ -18,15 +19,16 @@ import java.util.stream.Collectors;
 public class UserWithRolesResponse {
     List<String> roleNames;
     String email;
-    List<TicketDto> ticketDtos;
-    List<Coffee> coffees = new ArrayList<>();
+    List<TicketDto> tickets;
+    List<CoffeeDto> coffees;
 
     public UserWithRolesResponse(UserWithRoles userWithRoles){
         this.roleNames = userWithRoles.getRoles().stream().map(role -> role.toString()).collect(Collectors.toList());
         this.email = userWithRoles.getEmail();
-        List<Ticket> tickets = userWithRoles.getCustomerTickets().stream().map(CustomerTicket::getTicket).toList();
-        this.ticketDtos = tickets.stream().map(TicketDto::new).collect(Collectors.toList());
-        this.coffees = userWithRoles.getCoffees().stream().map(coffee -> new Coffee(coffee.getName(), coffee.getPrice())).collect(Collectors.toList());
+        List<Ticket> oldTickets = userWithRoles.getCustomerTickets().stream().map(CustomerTicket::getTicket).toList();
+        this.tickets = oldTickets.stream().map(TicketDto::new).collect(Collectors.toList());
+        List<Coffee> oldCoffees = userWithRoles.getCoffees();
+        this.coffees = oldCoffees.stream().map(CoffeeDto::new).collect(Collectors.toList());
     }
 
 }
